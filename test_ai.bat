784ed6bc4e0e171a211004d1edeae9276f148f00
arthur <arthur@ubuntu.(none)>
2012-11-01 16:27:23 -0200

    pif

--- test_ai.bat
+++ test_ai.bat
@@ -1,3 +1,3 @@
 copy .\src\AI-0132.swf .\deployment\swf
-del  .\deployment\AI-0132_SCO.zip
-"C:\Arquivos de Programas\7-Zip\7z.exe" a .\deployment\AI-0132_SCO.zip .\deployment\*.* -r
+del  .\deployment\AI-0132_PIF.zip
+"C:\Arquivos de Programas\7-Zip\7z.exe" a .\deployment\AI-0132_PIF.zip .\deployment\*.* -r
